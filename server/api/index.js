6cc4edd21d8e4b0d7d35c6ca7c146f64ae48802e
Jashan Bansal <jashanbansal7861@gmail.com>
2024-04-23 11:43:30 +0530

    the backend work

---
+++ server/api/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+
+
+const port = process.env.PORT;
+
+// create the backend connection 
+app.listen(port, ()=>{
+  console.log(`the backend server is running on ${port}`);
+})
+
+//the database string 
+const data = process.env.MONGO;
+
+// the database connection
+mongoose.connect(data).then(()=>{
+    console.log('connect');
+}).catch((err)=>{
+    console.log(err);
+})
